a2f93fac1a567936c382bb8d9ee5994bd4587b47
as66422 <aditya.ranglal.singhania@citi.com>
2017-09-29 18:50:04 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 
 var isPrompt = false, isAlert = false;
 var name = 'John Reese';
-console.log('Hello from the script js, ' + name);
+//console.log('Hello from the script js, ' + name);
 
 if(isPrompt) {
     var pr = prompt('Input a random value');
-    console.log(pr);
+    //console.log(pr);
 }
 
 if(isAlert) {
-    alert('You are doomed');
+    //alert('You are doomed');
 }
 
 var job = 'teacher'; 
 switch (job) {
     case 'teacher' :
-        console.log('He is a teacher');
+        //console.log('He is a teacher');
         break;
     default :
-        console.log('He is jobless');
+        //console.log('He is jobless');
         break;
 }
 
 function calculateAge (yearOfBirth) {
     var age = 2016 - yearOfBirth;
-    console.log(name + ' has age ' + age);
+    //console.log(name + ' has age ' + age);
 }
 
 var calculateRetirement = function(age) {
     var retirement = 65 - age;
-    console.log(name + ' has ' + retirement + 'years to retirement');
+    //console.log(name + ' has ' + retirement + 'years to retirement');
 }
 
 calculateAge(1994);
@@ -46,6 +46,28 @@ if(age===5) {}
 //Arrays
 var arr1 = ['aditya', 'seema'];
 var arr2 = new Array(1980, 'Aditya', 6740);
-console.log(arr1);
-console.log(arr2[0]);
+//console.log(arr1);
+//console.log(arr2[0]);
+
+//Objects
+var obj1 = new Object();
+var obj = {
+    name : 'Object',
+    age : '2017',
+    yearOfBirth : '0AD',
+    isMarried : true,
+    calculateFunc : function() {
+        return ('year : ' + this.yearOfBirth );
+    } 
+};
+//console.log(obj);
+//console.log(obj.age);
+//console.log(obj['age']);
+obj.place = 'Akola';
+//console.log(obj);
+console.log(obj.calculateFunc());
+
+//JavaScript basics theory
+/*
+
 
